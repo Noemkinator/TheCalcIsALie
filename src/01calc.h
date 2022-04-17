de011e28d882e466ffc9fbbc12d8a26b937c6845
FladosCamber <100806081+FladosCamber@users.noreply.github.com>
2022-04-17 16:44:15 +0200

    Add files via upload

---
+++ src/01calc.h
@@ -0,0 +1,13 @@
+#ifndef CALC_H_INCLUDED
+#define CALC_H_INCLUDED
+
+double sum(double a, double b);
+double minus(double a, double b);
+double multi(double a, double b);
+double delit(double a, double b);
+
+int factorial(int num);
+//double pow(double x, double y); from library math.h
+double root(double x, double y);
+
+#endif
